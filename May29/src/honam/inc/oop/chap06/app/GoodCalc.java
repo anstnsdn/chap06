3afffef547d20a5d104dcf8c10682a31c3eb62d2
owner <owner@owner-PC>
2017-05-29 10:14:17 +0900

    2017.05.29 수업내용 파일

---
+++ May29/src/honam/inc/oop/chap06/app/GoodCalc.java
@@ -0,0 +1,25 @@
+package honam.inc.oop.chap06.app;
+
+import honam.inc.oop.chap06.lib.calculator;
+
+public class GoodCalc extends calculator {
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int subtract(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double average(int[] a) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
