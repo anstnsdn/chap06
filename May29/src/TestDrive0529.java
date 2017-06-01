38f227f2b92d14760b4c7a37fd4b149c6d62a8c4
owner <owner@owner-PC>
2017-06-01 10:10:32 +0900

    0601추가수업한 내용

--- May29/src/TestDrive0529.java
+++ May29/src/TestDrive0529.java
@@ -1,9 +1,11 @@
+import honam.inc.oop.chap06.app.GoodCalc;
 
 public class TestDrive0529 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new GoodCalc();
+		
 	}
 
 }
--- May29/src/honam/inc/oop/chap06/app/GoodCalc.java
+++ May29/src/honam/inc/oop/chap06/app/GoodCalc.java
@@ -7,19 +7,19 @@ public class GoodCalc extends calculator {
 	@Override
 	public int add(int a, int b) {
 		// TODO Auto-generated method stub
-		return 0;
+		return a+b;
 	}
 
 	@Override
 	public int subtract(int a, int b) {
 		// TODO Auto-generated method stub
-		return 0;
+		return a-b;
 	}
 
 	@Override
 	public double average(int[] a) {
 		// TODO Auto-generated method stub
-		return 0;
+		return ;
 	}
 
 }
